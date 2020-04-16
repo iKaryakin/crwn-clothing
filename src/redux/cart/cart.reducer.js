8ceaaa8159dfb4869e0ea940b9a39844bd56a583
Ivan Karyakin <ivan.karyaking@gmail.com>
2020-04-16 14:29:43 +0300

    updating our application with reselect, replaced cart selectors with memoised selectors

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -9,9 +9,15 @@ const initialState = {
 const cartReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
-      return { ...state, hidden: !state.hidden };
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
     case CartActionTypes.ADD_ITEM:
-      return { ...state, cartItems: addItemToCart(state.cartItems, payload) };
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, payload),
+      };
 
     default:
       return state;
